5725714bcab50972e7a9f8d370c38f3c19a1b7d7
yanhaijing <yanhaijing@yeah.net>
2015-01-09 15:11:48 +0800

    添加新功能 1.添加UMD支持 2.将变量注入作用域，不再需要显示this 3.添加版本属性

--- template.js
+++ template.js
@@ -1,29 +1,50 @@
 /**
  * 模版引擎
  */
+;(function(root, factory) {
+    var template = factory(root);
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define('template', function() {
+            return template;
+        });
+    } else if (typeof exports === 'object') {
+        // Node.js
+        module.exports = template;
+    } else {
+        // Browser globals
+        var _template = root.template;
 
-;(function (root) {
-    function add() {
+        template.noConflict = function() {
+            if (root.template === template) {
+                root.template = _template;
+            }
 
+            return template;
+        };
+        root.template = template;
     }
+}(this, function(root) {
+    'use strict';
     function compile(tpl) {
         var reg = /<%(.*?)%>/g;
         var regblock = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g;
         var match;
         var point = 0;
-        var code = 'var r=[];\n';
+        var code = '';
         function add(line, js) {
-            js? code += line.match(regblock) ? line + '\n' : 'r.push(' + line + ');\n' :
-                    code += 'r.push("' + line.replace(/"/g, '\\"') + '");\n';        
+            js? code += line.match(regblock) ? line + '\n' : 'com_yanhaijing_templatejs_r.push(' + line + ');\n' :
+                    code += 'com_yanhaijing_templatejs_r.push("' + line.replace(/"/g, '\\"') + '");\n';        
         }
         while(match = reg.exec(tpl)){
             add(tpl.slice(point, match.index));
             add(match[1], true);
-            point = match.index + match[0].length;            
+            point = match.index + match[0].length;
         }
         add(tpl.substr(point, tpl.length - point));
-        code += 'return r.join("");';
-        return new Function(code.replace(/[\r\t\n]/g, ''));
+
+        code = '\nvar r = (function (com_yanhaijing_templatejs_data) {var com_yanhaijing_templatejs_str = "", com_yanhaijing_templatejs_r = [];\nfor(var key in com_yanhaijing_templatejs_data) {\ncom_yanhaijing_templatejs_str+=("var " + key + "=com_yanhaijing_templatejs_data[\'" + key + "\'];");\n}\neval(com_yanhaijing_templatejs_str);\n' + code + ';\nreturn com_yanhaijing_templatejs_r}(data));\nreturn r.join("");';
+        return new Function('data', code.replace(/[\r\t\n]/g, ''));
     }
     function template(tpl, data) {
         if (typeof tpl !== 'string') {
@@ -31,15 +52,15 @@
         }
 
         var fn = compile(tpl);
-
+        console.log(fn);
         if (Object.prototype.toString.call(data) !== '[object Object]') {
             return function (data) {
-                fn.call(data);
+                fn.call(null, data);
             };
         }
 
-        return fn.call(data);
+        return fn.call(null, data);
     }
-    var tpl = document.getElementById('tpl').innerHTML;
-    console.log(template(tpl, {name: 123}));
-}(window));
+    template.version = 0.1.0;
+    return template;
+}));
