e320845d153a7389b95c1fe4e298de712e8f6df6
yanhaijing <yanhaijing@yeah.net>
2019-10-21 16:30:19 +0800

    feat: add util

---
+++ packages/util/src/index.ts
@@ -0,0 +1,18 @@
+import { B, yan } from './test';
+
+import yan2 from './test';
+
+console.log(yan);
+console.log(yan2);
+
+const a = 1 + 1;
+const b = a;
+console.log(a);
+console.log(b);
+console.log(B);
+
+export function greeter(person: string): string {
+    return 'Hello, ' + person;
+}
+
+export const name = 'base';
