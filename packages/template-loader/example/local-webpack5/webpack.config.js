f9d720168e6e723cfa439ee48d5a9a1e6efc4d1c
yanhaijing <yanhaijing@yeah.net>
2022-08-15 18:48:01 +0800

    FEAT: add webpack 5 demo

---
+++ packages/template-loader/example/local-webpack5/webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+
+module.exports = {
+    entry: "./app.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tmpl/,
+                use: [
+                    {
+                        loader: __dirname + "/../../",
+                        options: {
+                            sTag: "<#",
+                            eTag: "#>",
+                            expression:
+                                'require("@templatejs/runtime").default',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
